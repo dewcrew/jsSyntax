a66c0d638b0b97c60e1a972d6e5fc9e657719cab
dewcrew <138113358+dewcrew@users.noreply.github.com>
2023-07-07 11:54:00 +0200

    update on syntax.js

--- syntax.js
+++ syntax.js
@@ -1,3 +1,13 @@
 //This is a portfoli project on JS Syntax
-let a = 5;
-console.log(a*10)
+
+
+const fuel = ["electricity", "gasoline", "diesel", "hybrid"];
+const carBrand = ["Saab", "Toyota", "Skoda", "BMW" ];
+const carPrice = ["affordable", "more than 500 000", "Do they even exist anymore?", "to expensive"];
+
+const selectFuel = fuel[Math.floor(Math.random()*fuel.length)]; 
+const selectCarBrand = carBrand[Math.floor(Math.random()*carBrand.length)];
+const selectCarPrice = carPrice[Math.floor(Math.random()*carPrice.length)]
+
+console.log( `You should buy a car that runs on ${selectFuel} , the brand should be ${selectCarBrand},
+    And you should spend ${selectCarPrice} kr`);
